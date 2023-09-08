fd4209104f3ca9c0d58a9359da04c3a96b6509f6
Zzrk <1252124617@qq.com>
2023-09-08 23:57:32 +0800

    feat: serial_windows

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-fn main() -> eframe::Result<()> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    egui_demo::start_puffin_server(); // NOTE: you may only want to call this if the users specifies some flag or clicks a button!
-    let native_options = eframe::NativeOptions {
-        drag_and_drop_support: true,
-        initial_window_size: Some(egui::vec2(400.0, 1000.0)),
-        ..Default::default()
-    };
+mod serial_windows;
 
-    eframe::run_native(
-        "egui demo",
-        native_options,
-        Box::new(|_cc| Box::new(egui_demo::ScreenshotApp::default())),
-    )
+fn main() -> eframe::Result<()> {
+    serial_windows::main()
+    // env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    // egui_demo::start_puffin_server(); // NOTE: you may only want to call this if the users specifies some flag or clicks a button!
+    // let native_options = eframe::NativeOptions {
+    //     drag_and_drop_support: true,
+    //     initial_window_size: Some(egui::vec2(400.0, 1000.0)),
+    //     ..Default::default()
+    // };
+
+    // eframe::run_native(
+    //     "egui demo",
+    //     native_options,
+    //     Box::new(|_cc| Box::new(egui_demo::ScreenshotApp::default())),
+    // )
 }
