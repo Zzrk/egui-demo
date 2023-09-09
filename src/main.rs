0fb73aaa7d98aef4e97b2b467728b1b21678198a
Zzrk <1252124617@qq.com>
2023-09-09 12:04:07 +0800

    feat: user_attention

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-mod serial_windows;
+// mod serial_windows;
+mod user_attention;
 
 fn main() -> eframe::Result<()> {
-    serial_windows::main()
+    // serial_windows::main()
+    user_attention::main()
+
     // env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     // egui_demo::start_puffin_server(); // NOTE: you may only want to call this if the users specifies some flag or clicks a button!
     // let native_options = eframe::NativeOptions {
@@ -15,6 +18,10 @@ fn main() -> eframe::Result<()> {
     // eframe::run_native(
     //     "egui demo",
     //     native_options,
-    //     Box::new(|_cc| Box::new(egui_demo::ScreenshotApp::default())),
+    //     Box::new(|cc| {
+    //         // egui 加载 svg 必须的 loaders
+    //         // egui_extras::loaders::install(&cc.egui_ctx);
+    //         Box::new(egui_demo::MyApp::new(cc))
+    //     }),
     // )
 }
