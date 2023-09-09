20fee5ff92e9f5a3edc5c8cb972ff984d27b98a5
Zzrk <1252124617@qq.com>
2023-09-09 12:19:41 +0800

    feat: custom_window_frame

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 // mod serial_windows;
-mod user_attention;
+// mod user_attention;
+mod custom_window_frame;
 
 fn main() -> eframe::Result<()> {
     // serial_windows::main()
-    user_attention::main()
+    // user_attention::main()
+    custom_window_frame::main()
 
     // env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     // egui_demo::start_puffin_server(); // NOTE: you may only want to call this if the users specifies some flag or clicks a button!
